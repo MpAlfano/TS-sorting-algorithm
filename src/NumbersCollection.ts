33736ca7b0f4d1786b5d4c49beb8199c42a6ca63
Mark Alfano <mpalfano4@gmail.com>
2022-08-18 20:51:55 -0400

    created reusable algorithm for sorting

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,22 @@
+export class NumbersCollection {
+  constructor(public data: number[]) {}
+
+  // get allows us to access length as a property instead of having to call the method length() see line 22
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex] > this.data[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.data[leftIndex];
+    this.data[leftIndex] = this.data[rightIndex];
+    this.data[rightIndex] = leftHand;
+  }
+}
+
+const collection = new NumbersCollection([1, 2, 3]);
+
+collection.length;
